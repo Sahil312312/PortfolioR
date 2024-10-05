e9b419df9641fa108cd868b0e2e8410da14fc33d
Sahil312312 <sahilshrivastava312312@gmail.com>
2024-10-05 19:53:10 +0530

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
 import { BrowserRouter } from "react-router-dom";
-import {About,Contact,Experience,Feedbacks,Hero,Navbar,StarsCanvas,Tech,Works} from './components'
-import { Analytics } from "@vercel/analytics/react"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <>
-    <Analytics>
       <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar/>
-          <Hero/>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <Feedbacks/>
-        <div className="relative z-0">
-          <Contact/>
-          <StarsCanvas/>
-        </div>
-      </div>
       </BrowserRouter>
-      </Analytics>
+      <Analytics /> {/* This should be outside BrowserRouter */}
     </>
   );
-}
+};
 
 export default App;
